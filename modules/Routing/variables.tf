a5ba9ef464695b783087bc128b6e8ab66f68df47
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 17:56:08 +0300

    Routing  module Added

---
+++ modules/Routing/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "internet_gateway_id" {
+  type = string
+}
+
+variable "nat_gateway_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs to associate with the public route table."
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs to associate with the private route table."
+  type = list(string)
+}
