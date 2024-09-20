550149f68b6f900bb864977504f378cd648fefd7
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 19:52:27 +0300

    formatting

--- modules/Routing/main.tf
+++ modules/Routing/main.tf
@@ -15,7 +15,7 @@ resource "aws_route_table" "private" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = var.nat_gateway_id
   }
 
@@ -25,13 +25,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count      = length(var.public_subnet_ids)
-  subnet_id     = var.public_subnet_ids[count.index]
+  count          = length(var.public_subnet_ids)
+  subnet_id      = var.public_subnet_ids[count.index]
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count      = length(var.private_subnet_ids)
-  subnet_id     = var.private_subnet_ids[count.index]
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
   route_table_id = aws_route_table.private.id
 }
--- modules/Routing/variables.tf
+++ modules/Routing/variables.tf
@@ -12,10 +12,10 @@ variable "nat_gateway_id" {
 
 variable "public_subnet_ids" {
   description = "List of public subnet IDs to associate with the public route table."
-  type = list(string)
+  type        = list(string)
 }
 
 variable "private_subnet_ids" {
   description = "List of private subnet IDs to associate with the private route table."
-  type = list(string)
+  type        = list(string)
 }
