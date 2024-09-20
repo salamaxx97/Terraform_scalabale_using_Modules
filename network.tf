9880c5a1da034de8c82462a3a8e31c628fe0fff8
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 16:08:08 +0300

    triying Subnets  module

--- network.tf
+++ network.tf
@@ -1,5 +1,11 @@
 module "vpc" {
-    source = "./vpc"
+    source = "./modules/vpc"
     vpc_cidr = var.vpc_cidr
     
+}
+module "subnets" {
+  source  = "./modules/Subnets"
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = var.subnets
 }
