b235035884bf17ec292ef0f54813ee2951f3bc98
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 17:56:47 +0300

    using nat igw routing  modules

--- network.tf
+++ network.tf
@@ -9,3 +9,24 @@ module "subnets" {
   vpc_id  = module.vpc.vpc_id
   subnets = var.subnets
 }
+
+module "igw" {
+  source = "./modules/InternetGateway"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "nat_gateway" {
+  source            = "./modules/NatGateway"
+  public_subnet_id = module.subnets.public_subnets[0]
+}
+
+
+module "routing" {
+  source              = "./modules/Routing"
+  vpc_id              = module.vpc.vpc_id
+  internet_gateway_id = module.igw.id
+  nat_gateway_id      = module.nat_gateway.id
+
+  public_subnet_ids   = module.subnets.public_subnets
+  private_subnet_ids  = module.subnets.private_subnets
+}
