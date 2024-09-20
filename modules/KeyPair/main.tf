d21aaf8f2cd329be892e324336c557a208dada42
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 19:45:21 +0300

    KeyPair module added

---
+++ modules/KeyPair/main.tf
@@ -0,0 +1,19 @@
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create a key pair to AWS
+resource "aws_key_pair" "kp" {
+  key_name   = var.key_name
+  public_key = tls_private_key.pk.public_key_openssh
+
+ # excute this command localy and save KeyPair to .ssh 
+  provisioner "local-exec" {
+    command = <<EOT
+    echo '${tls_private_key.pk.private_key_pem}' > ~/.ssh/${var.key_name}.pem
+    chmod 400 ~/.ssh/${var.key_name}.pem
+  EOT
+  }
+
+}
