b8aa6898d970c94266f267c2053119c165b13dfc
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 17:54:43 +0300

    Modifying Subnets module outputs to return list of public/private ids

--- modules/Subnets/output.tf
+++ modules/Subnets/output.tf
@@ -2,3 +2,13 @@ output "subnet_ids_by_name" {
   description = "Map of subnet Name to subnet IDs."
   value = { for subnet_key, subnet in aws_subnet.subnets : subnet_key=> subnet.id }
 }
+
+output "public_subnets" {
+  description = "List of IDs of public subnets."
+  value = [for subnet_key, subnet in aws_subnet.subnets : subnet.id if subnet.map_public_ip_on_launch == true]
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets."
+  value = [for subnet_key, subnet in aws_subnet.subnets : subnet.id if subnet.map_public_ip_on_launch == false]
+}
