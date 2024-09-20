54a3cba5dd9cb159858dce1971b506e7b6b75770
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-20 19:52:48 +0300

    formatting

--- modules/SecurityGroup/main.tf
+++ modules/SecurityGroup/main.tf
@@ -1,12 +1,12 @@
 resource "aws_security_group" "load_balancer_sg" {
-  name        = "${var.name}-lb-sg"
-  vpc_id      = var.vpc_id
+  name   = "${var.name}-lb-sg"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
   }
 
   tags = {
@@ -15,27 +15,27 @@ resource "aws_security_group" "load_balancer_sg" {
 }
 
 resource "aws_security_group" "private_instance_sg" {
-  name        = "${var.name}-private-sg"
-  vpc_id      = var.vpc_id
+  name   = "${var.name}-private-sg"
+  vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.load_balancer_sg.id]  # Allow HTTP from LB
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id] # Allow HTTP from LB
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]  # Allow SSH from Bastions
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id] # Allow SSH from Bastions
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1" # Allow all outbound traffic
   }
 
   tags = {
@@ -44,20 +44,20 @@ resource "aws_security_group" "private_instance_sg" {
 }
 
 resource "aws_security_group" "bastion_sg" {
-  name        = "${var.name}-bastion-sg"
-  vpc_id      = var.vpc_id
+  name   = "${var.name}-bastion-sg"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1" # Allow all outbound traffic
   }
 
   tags = {
