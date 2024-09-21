6bdab77d92887fcaaae5f11dac96c7a2d64c726f
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-21 07:23:45 +0300

    output.tf updated

--- output.tf
+++ output.tf
@@ -5,7 +5,7 @@ output "load_balancer_dns" {
 
 output "bastion_instance_public_ip" {
   value = module.ec2_instances.bastion_instance_public_ip
-} 
+}
 
 output "private_instance_ips" {
   value = module.ec2_instances.private_instance_ips
