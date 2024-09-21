50b157a986f01b71dc38963741f2180ad1523eb1
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-21 05:27:28 +0300

    Retrieve instances' ips as an output

--- modules/EC2Instances/output.tf
+++ modules/EC2Instances/output.tf
@@ -5,3 +5,11 @@ output "bastion_instance_ids" {
 output "private_instance_ids" {
   value = aws_instance.private[*].id
 }
+
+output "bastion_instance_public_ip" {
+  value = aws_instance.bastion[*].public_ip
+} 
+
+output "private_instance_ips" {
+  value = aws_instance.private[*].private_ip
+}
--- output.tf
+++ output.tf
@@ -2,3 +2,11 @@ output "load_balancer_dns" {
   description = "DNS name of the load balancer"
   value       = module.load_balancer.load_balancer_dns
 }
+
+output "bastion_instance_public_ip" {
+  value = module.ec2_instances.bastion_instance_public_ip
+} 
+
+output "private_instance_ips" {
+  value = module.ec2_instances.private_instance_ips
+}
